cec1e94af1bb1fe169c341b21777ba5cca961f65
deoagent <deoagent@gmail.com>
2021-09-03 13:16:37 +0800

    #setup and application setting

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    //CREATE API
+    public function createEmployee(Request $request){
+
+    }
+
+    //LIST API
+    public function listEmployees(){
+
+    }
+
+    //SINGLE DETAIL API
+    public function getSingleEmployee($id){
+
+    }
+
+    //UPDATE API
+    public function updateEmployee(Request $request, $id){
+
+    }
+
+    //DELETE API
+    public function deleteEmployee($id){
+
+    }
+
+}
